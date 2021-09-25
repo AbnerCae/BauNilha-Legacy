e5e76112d77e2814ef8e79c57e74a41f904e78b4
R4l7 <77628730+R4l7@users.noreply.github.com>
2021-09-25 13:15:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const aoijs = require('aoi.js');
 const bot = new aoijs.Bot({
+autoupdate: true
 prefix: ['$getServerVar[prefix]'],
-token: process.env.TOKEN
+token: process.env.TOKEN,
+intents: 'all'
 });
 bot.variables({
 prefix: 'b/'
