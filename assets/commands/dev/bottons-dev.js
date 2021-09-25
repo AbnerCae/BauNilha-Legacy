d89d91f2a03d38ee86edb8ef52a58e4031f563cd
R4l7 <77628730+R4l7@users.noreply.github.com>
2021-09-25 13:00:50 -0400

    Update bottons-dev.js

--- assets/commands/dev/bottons-dev.js
+++ assets/commands/dev/bottons-dev.js
@@ -1,13 +1,7 @@
-module.exports = [{
+module.exports = {
 name: 'bc',
 code: `
 $buttonCollector[$get[id];$authorID;1m;click;awaitclick;Only $userName can use this interaction,,64]
 $let[id;$apiMessage[$channelID;hi;;{actionRow:click me,2,1,click};;yes]]
 `
-}, {
-type: 'awaitedCommand',
-name: 'awaitclick',
-code: `
-$interactionReply[Hello;;;64]
-`
-}];
+};
