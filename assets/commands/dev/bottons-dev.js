2ab7a8e46f4a17e27935b24f3a4258068f9edd03
R4l7 <77628730+R4l7@users.noreply.github.com>
2021-09-25 13:14:12 -0400

    Delete bottons-dev.js

--- assets/commands/dev/bottons-dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-name: 'bc',
-code: `
-$buttonCollector[$get[id];$authorID;1m;click;awaitclick;Only $userName can use this interaction,,64]
-$let[id;$apiMessage[$channelID;hi;;{actionRow:click me,2,1,click};;yes]]
-`
-};
