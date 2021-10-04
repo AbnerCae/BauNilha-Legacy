9a16ae130ca40f1fe275b6d0db34565405bf7c45
R4l7 <77628730+R4l7@users.noreply.github.com>
2021-10-04 14:55:28 -0400

    Create help.js

---
+++ assets/commands/misc/help.js
@@ -0,0 +1,11 @@
+module.exports = {
+name: 'help',
+aliases: ['ajuda'],
+code: `
+👋 Olá! meu nome é $username[$clientID].
+Eu sou um bot criado com a intenção de te ajudar!
+Links Fixados:
+$description[Lista de Comandos: <>]
+$color[]
+`
+}
