6d93bf47e99b8833ec0f6ffe1ab273e441599778
R4l7 <77628730+R4l7@users.noreply.github.com>
2021-10-04 23:33:49 -0400

    Update daily.js
    Aviso de Quantidade na Bolsa (inventário)

--- assets/commands/economy/daily.js
+++ assets/commands/economy/daily.js
@@ -2,8 +2,8 @@ module.exports = {
 name: 'daily',
 aliases: ['diaria', 'diária'],
 code: `
-$reply[$messageID;💵 Você recebeu $random[15;100] zap zap tá moeto.]
-$setUserVar[money;$random[15;100]]
+$reply[$messageID;💧 Você recebeu $random[15;70] Mana.$if[$getUserVar[money]>=1]\n🎒 Você tem no total de $sum[$getUserVar[money];$random[15;70]]!] $elseIf$endIf]
+$setUserVar[money;$random[15;70]]
 $cooldown[1d;Espere %time% para pegar mais daily.]
 `
 }
