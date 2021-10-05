3f5b8c853c4355f940a04f2f93b4813d25464a4d
R4l7 <77628730+R4l7@users.noreply.github.com>
2021-10-04 23:36:01 -0400

    Update daily.js
    Abaixado o Cooldown

--- assets/commands/economy/daily.js
+++ assets/commands/economy/daily.js
@@ -2,8 +2,8 @@ module.exports = {
 name: 'daily',
 aliases: ['diaria', 'diária'],
 code: `
-$reply[$messageID;💧 Você recebeu $random[15;70] Mana.$if[$getUserVar[money]>=1]\n🎒 Você tem no total de $sum[$getUserVar[money];$random[15;70]]!] $elseIf$endIf]
+$reply[$messageID;💧 Você recebeu $random[15;70] Mana. $if[$getUserVar[money]>=1]\n🎒 Você tem no total de $sum[$getUserVar[money];$random[15;70]]!]$else$endIf]
 $setUserVar[money;$random[15;70]]
-$cooldown[1d;Espere %time% para pegar mais daily.]
+$cooldown[12h;Espere %time% para pegar mais daily.]
 `
 }
