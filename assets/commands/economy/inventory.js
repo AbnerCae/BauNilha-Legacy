14771bf1843a390baa85f290c36868cc9c7b1ceb
R4l7 <77628730+R4l7@users.noreply.github.com>
2021-10-04 23:43:46 -0400

    Update inventory.js

--- assets/commands/economy/inventory.js
+++ assets/commands/economy/inventory.js
@@ -3,7 +3,7 @@ name: 'inventory',
 aliases: ['inventario', 'inventário', 'i'],
 code: `
 $reply[$messageID;
-$if[$getUserVar[money]>=1]
+$if[$getUserVar[money]<=1]
 {field:💧 Mana:$getUserVar[money]:yes}
 $else
 {field:💁 Não achei nada no seu Inventário: Obtenha mana agora! usando *$getServerVar[prefix]daily*!}
