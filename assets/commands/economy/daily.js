39e8c5ffcc70b2aeecc81dee2dcb34d1692fc203
R4l7 <77628730+R4l7@users.noreply.github.com>
2021-10-04 23:43:14 -0400

    Update daily.js

--- assets/commands/economy/daily.js
+++ assets/commands/economy/daily.js
@@ -2,7 +2,7 @@ module.exports = {
 name: 'daily',
 aliases: ['diaria', 'diária'],
 code: `
-$reply[$messageID;💧 Você recebeu $random[15;70] Mana. $if[$getUserVar[money]>=1]\n🎒 Você tem no total de $sum[$getUserVar[money];$random[15;70]]!]$else$endIf]
+$reply[$messageID;💧 Você recebeu $random[15;70] Mana. $if[$getUserVar[money]<=1]\n🎒 Você tem no total de $sum[$getUserVar[money];$random[15;70]]!]$else$endIf]
 $setUserVar[money;$random[15;70]]
 $cooldown[12h;Espere %time% para pegar mais daily.]
 `
